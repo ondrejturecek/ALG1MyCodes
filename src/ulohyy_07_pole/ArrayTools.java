3835f6c15655e59f161929e96afcacdfeb2e466f
OndrejTurecek <ondrej.turecek@tul.cz>
2024-11-13 09:44:17 +0100

    add U14Prestupnzrok

--- src/ulohyy_07_pole/ArrayTools.java
+++ src/ulohyy_07_pole/ArrayTools.java
@@ -12,26 +12,40 @@ import static ulohyy_07_pole.LoadAray.loadArray;
  * @author Dagmar Malá
  */
 public class ArrayTools {
-    public static int soucet(int[] numbers){
+
+    public static int soucet(int[] numbers) {
         int sum = 0;
         for (int i = 0; i <= numbers.length - 1; i++) {
             sum = sum + numbers[i];
         }
         return sum;
     }
-    public static int maximalniHodnota(int[] numbers){
+
+    public static int maximalniHodnota(int[] numbers) {
         int max = numbers[0];
         for (int i = 0; i <= numbers.length - 1; i++) {
-            if(numbers[i] > max){
+            if (numbers[i] > max) {
                 max = numbers[i];
-            }     
+            }
         }
         return max;
     }
-    
+
+    public static int indexMin(int[] numbers) {
+        int min = numbers[0];
+        int index = 0;
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
-        int [] pole = loadArray();
-        int max = maximalniHodnota(pole);
-        System.out.println("max hodnoda =  " + max);
+        int[] pole = loadArray();
+        int index = indexMin(pole) + 1;
+        System.out.println("index min =  " + index);
     }
 }
