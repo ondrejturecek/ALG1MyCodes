321f87362f8be0579405080da8cea4f57b058dd5
OndrejTurecek <ondrej.turecek@tul.cz>
2024-11-13 09:30:39 +0100

    add U14Prestupnzrok

---
+++ src/ulohyy_07_pole/ArrayTools.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulohyy_07_pole;
+
+import static ulohyy_07_pole.LoadAray.loadArray;
+
+/**
+ *
+ * @author Dagmar Malá
+ */
+public class ArrayTools {
+    public static int soucet(int[] numbers){
+        int sum = 0;
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            sum = sum + numbers[i];
+        }
+        return sum;
+    }
+    public static int maximalniHodnota(int[] numbers){
+        int max = numbers[0];
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            if(numbers[i] > max){
+                max = numbers[i];
+            }     
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        int [] pole = loadArray();
+        int max = maximalniHodnota(pole);
+        System.out.println("max hodnoda =  " + max);
+    }
+}
